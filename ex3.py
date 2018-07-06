243fbc2d2c19d820e9684b8069338acc6bd90a08
Julia Boichentsova <ya.anomaly@gmail.com>
2018-07-06 16:00:30 +0100

    Exercise 3 Learning of how to working with basic math operations in Python

---
+++ ex3.py
@@ -0,0 +1,56 @@
+# Prints text specified in quotes on the screen
+print "I will now count my chickens:"
+
+# Calculates and prints the number of Hens
+# 30 is divided by 6 and then the result is added to 25
+print "Hens", 25 + 30 / 6
+
+# Calculates and prints the number of Roosters
+# Calculation of 100 - 25 * 3 % 4 is permormed as following:
+# 1) 25 * 3 = 75
+# 2) 75 % 4 = 3 (3 is the remainder of 75 divided by 4)
+# 3) 100 - 3 = 97
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints text "Now I will count the eggs:"
+print "Now I will count the eggs:"
+
+# Calculates the number of eggs and prints this number on the screen
+# Calculation of 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 is performed as following:
+# 1) 4 % 2 = 0
+# 2) 1 / 4 = 0.25 (but it will only use integer part, i.e. 0)
+# 3) 3 + 2 + 1 - 5 + 0 - 0 + 6 = 1 + 6 = 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Prints text specified in quotes on the screen
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculates left and right part, compares and
+# Prints result of comparison of 5 < -2
+print 3 + 2 < 5 - 7
+
+# Prints text specified in quotes on the screen
+# Along with he result of calculation of 3 + 2, i.e. 5
+print "What is 3 + 2?", 3 + 2
+
+# Prints text specified in quotes on the screen
+# Along with he result of calculation of 5 - 7, i.e. -2
+print "What is 5 - 7?", 5 - 7
+
+# Prints text specified in quotes on the screen
+print "Oh, that's why it's False."
+
+# Prints text specified in quotes on the screen
+print "How about some more."
+
+# Prints text specified in quotes on the screen
+# Along with the result of comparison 5 > -2 , i.e. True
+print "Is it greater?", 5 > -2
+
+# Prints text specified in quotes on the screen
+# Along with the result of comparison 5 >= -2 , i.e. True
+print "Is it greater or equal?", 5 >= -2
+
+# Prints text specified in quotes on the screen
+# Along with the result of comparison 5 <= -2 , i.e. False
+print "Is it less or equal?", 5 <= -2
