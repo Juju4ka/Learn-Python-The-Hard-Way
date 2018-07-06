006e0c469bd0ceebb2ca38c001d08c573ba91a53
Julia Boichentsova <ya.anomaly@gmail.com>
2018-07-06 16:52:52 +0100

    Exercise 5 More variables and Printing

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Guido van Rossum'
+age = 62
+height = 75 #inches
+weight = 185 #lbs
+eyes = 'Grey'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He is %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee" % teeth
+
+
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
