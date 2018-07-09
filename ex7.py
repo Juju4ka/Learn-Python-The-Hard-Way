68e59f73a2871c788e81e9bb36789cb84a46337c
Julia Boichentsova <ya.anomaly@gmail.com>
2018-07-09 13:14:19 +0100

    Exercise 7 More Printring * learnt how to print same text multiple times; * learnt how to join 2 print functions to make them print text in 1 line

---
+++ ex7.py
@@ -0,0 +1,56 @@
+# Prints text specified in quotes
+print "Mary had a little lamb."
+
+# Replaces %s with the word 'snow' and
+# Prints text "Its fleece was white as snow" on the screen
+print "Its fleece was white as %s." % 'snow'
+
+# Prints text specified in quotes
+print "And everywhere that Mary went."
+
+# Prints text specified in quotes (i.e. a dot) 10 times
+print "." * 10 # what'd that do?
+
+# Assigns a letter, specified in quotes, to a variable end1
+end1 ="C"
+
+# Assigns a letter, specified in quotes, to a variable end2
+end2 = "h"
+
+# Assigns a letter, specified in quotes, to a variable end3
+end3 = "e"
+
+# Assigns a letter, specified in quotes, to a variable end4
+end4 = "e"
+
+# Assigns a letter, specified in quotes, to a variable end5
+end5 = "s"
+
+# Assigns a letter, specified in quotes, to a variable end6
+end6 = "e"
+
+# Assigns a letter, specified in quotes, to a variable end7
+end7 = "B"
+
+# Assigns a letter, specified in quotes, to a variable end8
+end8 ="u"
+
+# Assigns a letter, specified in quotes, to a variable end9
+end9 = "r"
+
+# Assigns a letter, specified in quotes, to a variable end10
+end10 = "g"
+
+# Assigns a letter, specified in quotes, to a variable end11
+end11 = "e"
+
+# Assigns a letter, specified in quotes, to a variable end12
+end12 = "r"
+
+
+# watch that comma at the end. try removing it to see what happens
+
+# Comma at the end joins the following 2 lines,
+# so the text is printed in 1 line with a space between them
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
