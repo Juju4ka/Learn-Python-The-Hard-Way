31d0130f131f5555173154266786a989093ba8bd
Julia Boichentsova <ya.anomaly@gmail.com>
2018-07-09 13:55:41 +0100

    Exercise 9 Printing, Printing, Printing * learnt how to print text from a new line using \n and 3 double-quotes

---
+++ ex9.py
@@ -0,0 +1,23 @@
+# Assigns text to the variable days
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# Assigns text to a variable months
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Prints text specified in quotes 
+# and text, specified in variable days, in 1 line
+print "Here are the days: ", days
+
+# Prints text, specified in quotes, 
+# and text specified in the variable months
+# Prints each month from a new line because of \n
+print "Here are the months: ", months
+
+# Prints text specified in quotes exactly as we wrote it,
+# i.e. prints every line as a new line
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
